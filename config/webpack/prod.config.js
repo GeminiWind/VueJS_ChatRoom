3c392b180662402054984f21c980fb7422688e55
kominam <kominam.2511@gmail.com>
2017-11-15 13:26:58 +0700

    Fix webpack output folder

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -22,25 +22,6 @@ module.exports = merge(baseConfig, {
           use: ['css-loader', 'sass-loader'],
         }),
       },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      }, {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
-      }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
-      }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader?limit=10000&mimetype=image/svg+xml',
-      }, {
-        test: /\.(png|gif|jpg|svg|jpeg)$/i,
-        loader: 'file-loader?name=images/[name].[ext]',
-      },
     ],
   },
   plugins: [
